469c10913a2e87ae6ea30436536d43368107f997
Filipo <filgreen@pm.me>
2025-04-22 22:05:01 +0200

    Customize neotree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -10,5 +10,11 @@ return {
         end,
       },
     },
+    filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+      },
+    },
   },
 }
