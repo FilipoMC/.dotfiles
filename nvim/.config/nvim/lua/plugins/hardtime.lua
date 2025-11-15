17e98cfc4866aa0c40dbb419aac904d9ee042a44
Filipo <filgreen@pm.me>
2025-11-15 14:21:36 +0100

    Neovim
    Leap, recover, hardtime

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -2,6 +2,9 @@ return {
   "m4xshen/hardtime.nvim",
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim" },
+  keys = {
+    { "<leader>co", "<cmd>Hardtime toggle<cr>", desc = "Toggle hardtime" },
+  },
   opts = {
     disable_mouse = false,
     -- restriction_mode = "hint",
