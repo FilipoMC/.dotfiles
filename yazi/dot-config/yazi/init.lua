c430a8f51c4d57f0d2aa1232cf2b40dc298ebe8d
Filipo <filgreen@pm.me>
2026-05-29 13:58:48 +0200

    Yazi: Add plugins

--- yazi/dot-config/yazi/init.lua
+++ yazi/dot-config/yazi/init.lua
@@ -2,3 +2,8 @@ require("gvfs"):setup({
 	save_path = os.getenv("HOME") .. "/.local/state/yazi/gvfs.private",
 	save_path_automounts = os.getenv("HOME") .. "/.local/state/yazi/gvfs_automounts.private",
 })
+
+require("yafg"):setup({
+	editor = "nvim",
+	file_arg_format = "+{row} {file}",
+})
