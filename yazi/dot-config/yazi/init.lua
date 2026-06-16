1a32b12e038eec81650e394c40f5a555ed6c0a2b
Filipo <filgreen@pm.me>
2026-06-16 15:47:53 +0200

    Yazi: Switch gvfs for mount, git status, smart filter

--- yazi/dot-config/yazi/init.lua
+++ yazi/dot-config/yazi/init.lua
@@ -1,6 +1,5 @@
-require("gvfs"):setup({
-	save_path = os.getenv("HOME") .. "/.local/state/yazi/gvfs.private",
-	save_path_automounts = os.getenv("HOME") .. "/.local/state/yazi/gvfs_automounts.private",
+require("git"):setup({
+	order = 1500,
 })
 
 require("yafg"):setup({
