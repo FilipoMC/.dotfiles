7bee9b65fb00115fda5bcc3605bed4239f0f0584
Filipo <filgreen@pm.me>
2026-07-06 17:51:15 +0200

    Hypr: Clean up config
    - Moved scripts to ~/.local/bin
    - Renamed configs to hyprland
    - Moved main config from hyprland.lua to hyprland/main.lua

---
+++ hypr/dot-config/hypr/hyprland/main.lua
@@ -0,0 +1,83 @@
+hl.config({
+	general = {
+		gaps_in = 5,
+		gaps_out = 10,
+
+		border_size = 1,
+
+		col = {
+			active_border = { colors = { "rgba(a6e3a1ee)" } },
+			inactive_border = "rgba(595959aa)",
+			nogroup_border_active = { colors = { "rgba(a6e3a1ee)" } },
+			nogroup_border = "rgba(595959aa)",
+		},
+
+		layout = "dwindle",
+	},
+
+	decoration = {
+		rounding = 10,
+		rounding_power = 2,
+
+		-- Opacity of active/inactive windows
+		active_opacity = 1.0,
+		inactive_opacity = 1.0,
+
+		shadow = {
+			enabled = true,
+			range = 4,
+			render_power = 3,
+			color = "rgba(1a1a1aee)",
+		},
+
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 1,
+			vibrancy = 0.1696,
+		},
+	},
+
+	dwindle = {
+		preserve_split = true,
+	},
+
+	misc = {
+		disable_hyprland_logo = 1,
+	},
+
+	input = {
+		kb_layout = "pl, ru",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
+
+		numlock_by_default = true,
+
+		follow_mouse = 1,
+	},
+})
+
+hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
+
+	suppress_event = "maximize",
+})
+
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
+
+	no_focus = true,
+})
