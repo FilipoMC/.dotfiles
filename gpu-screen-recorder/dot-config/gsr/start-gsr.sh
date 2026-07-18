84d8ca31a76f604f4e19719d06190c18fbc6a28a
Filipo <filgreen@pm.me>
2026-07-19 01:12:40 +0200

    GSR: Change start script options

--- gpu-screen-recorder/dot-config/gsr/start-gsr.sh
+++ gpu-screen-recorder/dot-config/gsr/start-gsr.sh
@@ -7,17 +7,16 @@ output_dir=~/Videos/gpu-screen-rec/
 mkdir -p "$output_dir"
 
 gpu-screen-recorder -r 240 \
-  -w screen -a default_output \
+  -w screen -a app-inverse:audio-src \
   -bm vbr -q medium \
   -fm vfr -f 24 \
   -k hevc \
   -s 2580x1080 \
   -c mp4 -o "$output_dir" \
   -sc ~/.config/gsr/on_clip.sh \
-  -fallback-cpu-encoding yes \
-  -replay-storage disk
+  -fallback-cpu-encoding yes
 
-# -
+# record whole screen, exclude spotify audio
 # const bitrate, 6mbps
 # const fps, 24
 # codec
